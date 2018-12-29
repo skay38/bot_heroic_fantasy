0e056bd49ccca673e36c4b23035e9f90ccca19bf
Hugo Durieu <hugo.durieu@gmail.com>
2018-12-29 13:55:46 +0100

    Update bot_hf.py

--- bot_hf.py
+++ bot_hf.py
@@ -393,7 +393,7 @@ def plus_facile(test_3):
 test=[0]*24
 @bot.command()
 async def productions(i,nb):
-    """Allow to know the best number of productions to do"""
+    """Allow to know the best number of productions"""
     h=plus_facile(list_terr2(i-1,nb,i-1,test))
     message=""
     for k in h:
