de8d2194536aa5a8b14ca47339de3ac620c1119f
skay38 <hugo.durieu@gmail.com>
2019-05-04 19:07:19 +0200

    Update bot_hf.py

--- bot_hf.py
+++ bot_hf.py
@@ -108,9 +108,8 @@ def afficher(game):
 @bot.command()
 async def code_names(ctx,espio1, espio2):
     """Allow to play to Code Names"""
-    print(espio1[3:-1])
-    espion1=bot.get_user(espio1[3:-1])
-    espion2=bot.get_user(espio2[3:-1])
+    espion1=bot.get_user(int(espio1[3:-1]))
+    espion2=bot.get_user(int(espio2[3:-1]))
     chan=ctx.channel
     game=code_names_init()
     a,grille=grille_jeu_init()
