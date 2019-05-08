81d4b2cd5aae3abc93cd40c7538ec53203b031b4
skay38 <hugo.durieu@gmail.com>
2019-05-08 12:58:54 +0200

    Update bot_hf.py

--- bot_hf.py
+++ bot_hf.py
@@ -678,7 +678,7 @@ async def send_all(ctx):
             test=1
     if test==0:
         return
-    await message_author.send(discord.File(open("messages.txt",'rb')))
+    await message_author.send(file=discord.File(open("messages.txt",'rb')))
     await message_channel.send("done !")
 
 @bot.command()
